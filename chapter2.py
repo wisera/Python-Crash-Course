8fdf8ecf9e6eb95c6fef6c1078bc518a5b3a1db5
wisera <luansurfer@hotmail.com>
2021-10-05 11:14:48 -0300

    test

---
+++ chapter2.py
@@ -0,0 +1,2 @@
+#Use a variabel to represent a person's name, and priont a message to that person. Your message should be simple, such as, "Hello Eric, would like to learn some python today?"
+name = Eric
