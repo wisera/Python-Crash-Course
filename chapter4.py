b1ba56f94519645c93bce7c862bcbf6ab2ab0f26
wisera <luansurfer@hotmail.com>
2021-10-06 17:25:50 -0300

    l

--- chapter4.py
+++ chapter4.py
@@ -53,4 +53,44 @@ print(cubes)
 cubes_comp = [number**3 for number in range(1,11)]
 print(cubes_comp)
 
+# using the created list best_pizzas, do the following:
+# print the message The first theree intems in the list are:. Then use a slice to print the first three items from that list
+best_pizzas = ['siciliana', 'burrata', 'pepperoni']
+print(f'The first three items in the list are: \n{best_pizzas[:3]}')
+
+#print the message Three items from the middle of the list are: Use slice
+best_pizzas.append('bacon')
+best_pizzas.append('mushroom')
+
+print(f'The three items from the middle are: \n{best_pizzas[1:4]}')
+
+#print the message Three last items...
+
+print(f'The last three are: \n{best_pizzas[-3:]}')
+
+# Make a copy of the best_pizzas, and call it friend_pizzas
+
+friend_pizzas = best_pizzas[:]
+print(friend_pizzas)
+
+# add a new pizza to the original list
+# add a different pizza to the friend_pizzas
+
+best_pizzas.append('parma')
+friend_pizzas.append('shizzle')
+
+# prove that you have 2 separate lists. Print a message My favorite pizzas are:, and then use a for loop to print the first list.
+# Print the message My frind's favorite pizzas are:. amd the use a for loop to print the second list. 
+# Make sure each new pizza is stored in the appropriate list
+print('My favorite pizzas are:')
+for pizza in best_pizzas:
+    print(pizza)
+
+print("My friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
+
+
+
+
 
