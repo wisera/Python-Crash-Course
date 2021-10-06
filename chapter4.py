bd40d43355e84decf6df9b2ad0aa5f7687f301b0
wisera <luansurfer@hotmail.com>
2021-10-06 12:03:21 -0300

    lists

--- chapter4.py
+++ chapter4.py
@@ -9,3 +9,48 @@ for pizza in best_pizzas:
     print(f'I love {pizza} pizza')
 
 print('These are my favourite pizza flavors')
+
+#LIST COMPREHENSIONS
+
+# Use a for loop to orint the numbers from 1 to 20
+
+for number in range(1,21):
+    print(number)
+
+# now print a list of numbers from 1 to 200
+
+my_list = list(range(1,201))
+print(my_list)
+
+# now use min() and max() to make sure your list actually starts at one and ends at 200
+#also use sum()
+
+print(min(my_list))
+print(max(my_list))
+print(sum(my_list))
+
+# use the thrid argument of the range() function to make a list of odd numbers from 1 to 20. Use a for loop to print each number
+
+odd_numbers = range(1,21,2)
+
+for number in odd_numbers:
+    print(number)
+
+# make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in your list
+
+multiples_of_3 = list(range(3,31,3))
+for number in multiples_of_3:
+    print(number)
+
+# make a list of the first 10 cubes, ex.:(2**3), from 1 to 10, and use a for loop to print out each value
+cubes = []
+for number in range(1,11):
+    cubes.append(number**3)
+print(cubes)
+
+# use a list comrpehension to generate a list of the first 10 cubes
+
+cubes_comp = [number**3 for number in range(1,11)]
+print(cubes_comp)
+
+
