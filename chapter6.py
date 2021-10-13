fd0d7f2940ca676b0cdfe4635efdfcdd36a4ac77
wisera <luansurfer@hotmail.com>
2021-10-13 12:39:11 -0300

    using lists and dicts in while loop

--- chapter6.py
+++ chapter6.py
@@ -57,3 +57,69 @@ elif age <= 3 or age < 12:
     print('Your ticket is $10')
 else:
     print('Your ticket is $15')
+
+# make a lit called sandwich_orders and fill it with the names of various sandwiches
+# Then make an empty list called finished_sandwiches.
+# Loop through the list of sandwich oreders and print a message for each order, such as I mde your tuna sandwich
+# as each sandwich is amde, move it to the list of finished sandwiches.
+# After all the sandwiches have been made, print a message listing each sandwich that was made
+
+sandwich_orders = ['tuna', 'blt', 'parma', 'cheese', 'salami']
+
+finished_sandwiches = []
+
+while sandwich_orders:
+    sandwich = sandwich_orders.pop()
+    print(f'I made your {sandwich} sandwich')
+    finished_sandwiches.append(sandwich)
+
+print(f'The following sandwiches have been made: ')
+
+for sandwich in finished_sandwiches:
+    print(f'{sandwich}')
+
+# using the list sandwich_orders from last exercise, make sure the sandwich 'parma' appears in the list at leat 3 times
+# add code near the beggining of your program to print a message saying the deli has run out of parma,
+# and then use a while loop to remove all occurences of 'parma' from sandwich_orders
+# make sure no parma sandwiches end up in finished_sandwiches
+
+sandwich_orders = ['tuna', 'parma', 'blt', 'parma', 'cheese', 'parma', 'salami']
+
+finished_sandwiches = []
+
+print('sorry we ran out of parma')
+
+while 'parma' in sandwich_orders:
+    sandwich_orders.remove('parma')
+
+while sandwich_orders:
+    sandwich = sandwich_orders.pop()
+    print(f'I made your {sandwich} sandwich')
+    finished_sandwiches.append(sandwich)
+
+print(f'The following sandwiches have been made: ')
+
+for sandwich in finished_sandwiches:
+    print(f'{sandwich}')
+
+# write a program that polls users about their dream vacation.
+# write a prompt similar to if you could visit one place in the world, where would you go?
+# include a vlock of code that prints the results to the poll
+
+dream_vacation = {}
+
+poll_active = True
+
+while poll_active:
+    name = input('Hello, whats your name?')
+    response = input('If you could visit one place in the world, where you you go?')
+    dream_vacation[name] = response
+
+    repeat = input(' would another person like to share? ( write quit to stop )')
+    if repeat == 'quit':
+        poll_active = False
+
+print('The poll results are: ')
+
+for name, response in dream_vacation.items():
+    print(f'{name.title()} would like to go to {response.title()}')
