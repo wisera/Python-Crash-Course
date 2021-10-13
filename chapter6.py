979657e808c5e1f4953cfe803af615e346900cd6
wisera <luansurfer@hotmail.com>
2021-10-12 21:54:02 -0300

    while

--- chapter6.py
+++ chapter6.py
@@ -24,3 +24,36 @@ if number % 10 == 0:
     print(f'{number} is a multiple of 10')
 else:
     print(f'{number} is not a multiple of 10')
+
+# write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value.
+# as they enter each topping, print a message saying you will add that topping to their pizza
+
+while True:
+    topping = input("enter your favorite pizza toppings \n (enter 'quit' to stop)")
+    if topping == 'quit':
+        break
+
+# or
+
+active = True
+
+while active:
+    topping = input("enter your favorite pizza toppings \n (enter 'quit' to stop)")
+    if topping == 'quit':
+        active = False
+
+
+# a movie theater charges different ticket prices depending on a person's age. 
+# If a person is under the age 0f 3, the ticket is free
+#  if they are between 3 and 12, the ticket is $10
+# if they are over age 12, the ticket is $15
+# write a loop in which you ask users their age, and then tell them the cost of their movie ticket
+
+age = int(input('How old are you? '))
+
+if age < 3:
+    print('Your ticket is free')
+elif age <= 3 or age < 12:
+    print('Your ticket is $10')
+else:
+    print('Your ticket is $15')
