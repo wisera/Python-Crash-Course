006d8a08bc40270f88bc0c9670d5b18800bcdbf3
wisera <luansurfer@hotmail.com>
2021-10-14 17:10:08 -0300

    finished writing to files w/ python exercises

--- chapter10-files-and-exceptions.py
+++ chapter10-files-and-exceptions.py
@@ -28,3 +28,34 @@ for line in lines:
 with open(file_name) as file_object:
     for line in file_object:
         print(line.replace('Python','C').rstrip())
+
+# write a program that prompts the user for their name.
+# when they respond, write their name to a file called guest.txt
+
+name = input('tell me ur name: ')
+
+with open('guest.txt', 'w') as file_object:
+    file_object.write(name)
+
+with open('guest.txt') as file_object:
+    contents = file_object.read()
+    print(contents)
+
+# write a while loop that prompts users for their name.
+# when they enter their name, print a greeting to the screen and add a line recording their visit in a file called guest_book.txt
+# make sure each entry appears on a new line in the file
+
+while True:
+    print('Tell me your name ( press q to quit )')
+    name = input('name: ')
+    if name == 'q':
+        break
+    print(f'Welcome {name.title()}')
+    
+    with open('guest_book.txt', 'a') as file_object:
+        file_object.write(f'{name}\n')
+
+with open('guest_book.txt') as file_object:
+    contents = file_object.read()
+    print(contents)
+
